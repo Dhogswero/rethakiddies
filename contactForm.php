f5325b7b71763175a78d53fbfff9fe8d2677cb4e
Dhogswero <48290832+Dhogswero@users.noreply.github.com>
2023-03-09 03:01:56 +0200

    Add files via upload

---
+++ contactForm.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['submit'])){
+
+	$_name = $_POST['names'];
+	$_email = $_POST['emails'];
+	$_message = $_POST['messages'];
+
+	$mailTo = "msizahope@edhope.co.za";
+	$headers = "From: ".$_email;
+	$txt = "You have an message from: ".$_name.".\n\n".$message;
+
+	mail($mailTo, $txt, $headers);
+	header("Location: index.html");
+}
+
+
+?>
